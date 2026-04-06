49116264f0f93454f64b275bec18d71d2da69f1d
bdutt001 <bdutt001@odu.edu>
2026-04-06 08:28:04 -0400

    db connection test 2

--- Project/server/database/connection.js
+++ Project/server/database/connection.js
@@ -8,11 +8,9 @@ console.log("USER:", process.env.SMTP_USER);
 console.log("PASS exists:", !!process.env.SMTP_PASS);
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
+  host: process.env.DB_HOST || process.env.MYSQL_ADDON_HOST,
+  user: process.env.DB_USER || process.env.MYSQL_ADDON_USER,
+  password: process.env.DB_PASSWORD || process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQL_ADDON_DB,
+  port: process.env.DB_PORT || process.env.MYSQL_ADDON_PORT || 3306,
 });
