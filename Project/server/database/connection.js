1132845e5dd76a594cc913afd9fad31278538a31
bdutt001 <bdutt001@odu.edu>
2026-04-06 01:50:17 -0400

    render error

--- Project/server/database/connection.js
+++ Project/server/database/connection.js
@@ -1,6 +1,12 @@
 import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
+console.log("SMTP CONFIG:");
+console.log("HOST:", process.env.SMTP_HOST);
+console.log("PORT:", process.env.SMTP_PORT);
+console.log("USER:", process.env.SMTP_USER);
+console.log("PASS exists:", !!process.env.SMTP_PASS);
+
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
