a3e52bb9845fdf2ed0e23a172624cbae735ea055
bdutt001 <bdutt001@odu.edu>
2026-04-06 09:18:01 -0400

    smtp error att 6

--- Project/server/route/user.js
+++ Project/server/route/user.js
@@ -7,11 +7,13 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465,
+  // secure: Number(process.env.SMTP_PORT) === 465,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  requireTLS: true,
 });
 
 const user = Router();
