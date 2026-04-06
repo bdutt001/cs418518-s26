b613696c64b42dcac82fcabfed009fb47d317e31
bdutt001 <bdutt001@odu.edu>
2026-04-06 01:17:18 -0400

    firebase deploy

--- Project/server/index.js
+++ Project/server/index.js
@@ -1,4 +1,6 @@
+import cookieParser from 'cookie-parser';
 import express from "express";
+import session from "express-session";
 import 'dotenv/config';
 import users from "./route/user.js"
 import advisingRoutes from "./route/advising.js";
@@ -16,15 +18,34 @@ const myLogger = function(req,res,next){
     next()
 }
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(
     cors({
         origin:process.env.FE_ORIGIN,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type"],
     }),
-)
+);
 
-app.use(express.json());
+app.set("trust proxy", 1);
+
+app.use(
+    session({
+        secret: process.env.SESSION_TICKET || "secretkey",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: true,
+            sameSize: "none",
+            maxAge: 1000 * 60 * 60
+        },
+    })
+);
+
+
 app.use(myLogger);
 app.use('/user', users);
 app.use("/api/advising", advisingRoutes);
