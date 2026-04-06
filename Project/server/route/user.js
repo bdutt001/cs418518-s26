aa6e3de01583ab4c28803e841d871c0774179642
bdutt001 <bdutt001@odu.edu>
2026-04-06 01:54:07 -0400

    render error att 2

--- Project/server/route/user.js
+++ Project/server/route/user.js
@@ -12,6 +12,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 const user = Router();
