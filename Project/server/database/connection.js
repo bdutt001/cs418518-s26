0cedffe8a59e5b99e642180ab1d332f3c0616f81
bdutt001 <bdutt001@odu.edu>
2026-04-06 00:11:00 -0400

    render deploy attempt 2

--- Project/server/database/connection.js
+++ Project/server/database/connection.js
@@ -1,10 +1,20 @@
 import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME
-})
+export const connection = async () => {
+    try {
+        const createConnection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            // port: process.env.DB_PORT || 3306 
+        });
 
-export {connection};
+        console.log("✅ Connected to database");
+        return createConnection;
+    } catch (err) {
+        console.error("❌ Database connection failed:", err);
+        throw err;
+    }
+};
